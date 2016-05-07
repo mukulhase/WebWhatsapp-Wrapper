6af63b2da563113011fd76dba60e0f4c3916d560
mukulhase <mukulhase@gmail.com>
2016-05-07 12:56:04 +0530

    second

--- webwhatsapp.py
+++ webwhatsapp.py
@@ -1,25 +1,44 @@
-from bs4 import BeautifulSoup
 from selenium import webdriver
 import selenium
 from selenium.webdriver.common.by import By
 import time
 import os
 driver = webdriver.Firefox()
-url="web.whatsapp.com"
+url="http://web.whatsapp.com"
 selectors = {
-    'qrCode':"#window > div.entry-main > div.qrcode > img"
+    'firstrun':"#wrapper",
+    'qrCode':"#window > div.entry-main > div.qrcode > img",
+    'mainPage':".app.two",
+    'chatList':".infinite-list-viewport"
 }
-def init:
+def init():
     driver.get(url)
-    try:
-        element = driver.find_element_by_css_selector(selectors['qrCode'])
-    except:
-        firstrun()
+    driver.implicitly_wait(10)
+    while True:
+        try:
+            time.sleep(1)
+            element = driver.find_element_by_css_selector(selectors['mainPage'])
+        except:
+            firstrun()
+            continue
+        break
+    run()
 
 def firstrun():
-    
+    print "first run"
+    screen = driver.save_screenshot('temp.png')
+    ok = input("waiting")
+
+    pass
+
+def press_send():
+    pass
+
+def enter_message():
 
 def run():
-    
+
+    pass
 
 init()
+driver.quit()
