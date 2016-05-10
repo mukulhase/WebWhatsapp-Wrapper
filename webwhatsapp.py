c4336e9db49262de3c9e9df856ea427c22f19cb0
Adarsh <adarshsanjeev@gmail.com>
2016-05-10 03:41:20 +0300

    Corrected select contact

--- webwhatsapp.py
+++ webwhatsapp.py
@@ -13,6 +13,7 @@ SELECTORS = {
     'messageList':"#main > div > div:nth-child(1) > div > div.message-list",
     'unreadMessageBar':"#main > div > div:nth-child(1) > div > div.message-list > div.msg-unread",
     'searchBar':"#side > div.search-container > div > label > input",
+    'searchCancel':".icon-search-morph",
     'chats':".infinite-list-item",
     'chatBar':'div.input',
     'sendButton':'button.icon:nth-child(3)',
@@ -21,6 +22,7 @@ SELECTORS = {
 driver = webdriver.Firefox()
 
 def init():
+    "Initialises the browser"
     driver.get(URL)
     driver.implicitly_wait(10)
     while True:
@@ -39,19 +41,42 @@ def firstrun():
     ok = input("waiting")
 
 def press_send():
+    "Presses the send button"
     driver.find_element_by_css_selector(SELECTORS['sendButton']).click()
 
 def enter_message(message):
+    "Enters the message onto the chat bar"
     driver.find_element_by_css_selector(SELECTORS['chatBar']).send_keys(message)
 
-def select_contact(contact):
+def select_contact(contact, entry = None):
+    """
+    Searches for the contact, as either name or number. If multiple exists, returns the
+    'entry'th contact. If entry is not sent, return all the rows.
+    """
+
+    # Focusing before sending keys solves many problems
+    driver.find_element_by_css_selector(SELECTORS['searchBar']).click()
+
     element = driver.find_element_by_css_selector(SELECTORS['searchBar']).send_keys(contact)
     time.sleep(1)
-    result = get_user_list()
-    a = result.find_elements_by_css_selector(SELECTORS['chats'])
-    if len(result.find_elements_by_css_selector(SELECTORS['chats']))>0:
+
+    try:
+        result = get_user_list()
+    except NoSuchElementException:
+        return False
+
+    # To get the most recent chat first, we reverse it
+    contacts = result.find_elements_by_css_selector(SELECTORS['chats'])[::-1]
+    time.sleep(1)
+
+    if len(contacts) == 1:
         result.find_elements_by_css_selector(SELECTORS['chats'])[0].click()
-    return a
+    elif entry is None:
+        driver.find_element_by_css_selector(SELECTORS['searchCancel']).click()
+        return contacts
+    else:
+        contacts[entry].click()
+    return True
 
 def get_user_list():
     element = driver.find_element_by_css_selector(SELECTORS['chatList'])
