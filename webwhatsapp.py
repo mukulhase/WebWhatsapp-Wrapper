d32c1539bfa56b5b103aea7468ce40c31c3f8a97
mukul hase <mukulhase@gmail.com>
2016-06-05 00:19:22 +0530

    `Placed script inside python file

--- webwhatsapp.py
+++ webwhatsapp.py
@@ -104,7 +104,39 @@ class WhatsAPIDriver():
         pass
 
     def view_unread(self):
-        Store =  self.driver.execute_script(open("./script.js").read())
+        script = '''
+        var Chats = Store.Chat.models;
+        if (!('last_read' in window)) {
+            this.last_read = {};
+            for (chat in Chats) {
+                window.last_read[Chats[chat]._values.formattedTitle] = Math.floor(Date.now() / 1000);
+            }
+        }
+        var Output = [];
+        for (chat in Chats) {
+            var temp = {};
+            temp.contact = Chats[chat]._values.formattedTitle;
+            temp.messages = [];
+            var messages = Chats[chat].msgs.models;
+            for (var i=messages.length-1;i>=0;i--) {
+                if (messages[i]._values.t <= last_read[Chats[chat]._values.formattedTitle]) {
+                    break;
+                } else {
+                    temp.messages.push(
+                        {
+                            message: messages[i]._values.body,
+                            timestamp: messages[i]._values.t
+                        }
+                    );
+                }
+            }
+            last_read[Chats[chat]._values.formattedTitle] = messages[messages.length-1]._values.t;
+            if(temp.messages.length>0)
+                Output.push(temp);
+        }
+        return Output;
+        '''
+        Store =  self.driver.execute_script(script)
         return Store
 
     def __unicode__(self):
