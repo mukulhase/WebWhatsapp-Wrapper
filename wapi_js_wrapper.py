4c40e8b4469685e085dc67840da774d1bb366810
Meshulam Silk <moomoohk@ymail.com>
2017-09-02 20:55:59 +0300

    Documentation

--- wapi_js_wrapper.py
+++ wapi_js_wrapper.py
@@ -2,10 +2,21 @@ import os
 
 
 class WapiJsWrapper(object):
+    """
+    Wraps JS functions in window.WAPI for easier use from python
+    """
+
     def __init__(self, driver):
         self.driver = driver
 
     def __getattr__(self, item):
+        """
+        Finds functions in window.WAPI
+
+        :param item: Function name
+        :return: Callable function object
+        :rtype: JsFunction
+        """
         wapi_functions = dir(self)
 
         if item not in wapi_functions:
@@ -14,6 +25,11 @@ class WapiJsWrapper(object):
         return JsFunction(item, self.driver)
 
     def __dir__(self):
+        """
+        Reloads wapi.js and returns its functions
+
+        :return: List of functions in window.WAPI
+        """
         with file(os.path.join("js", "wapi.js"), "rb") as script:
             self.driver.execute_script(script.read())
 
@@ -21,10 +37,24 @@ class WapiJsWrapper(object):
 
 
 class JsArg(object):
+    """
+    Represents a JS function argument
+    """
+
     def __init__(self, obj):
+        """
+        Constructor
+
+        :param obj: Python object to represent
+        """
         self.obj = obj
 
     def __str__(self):
+        """
+        Casts self.obj from python type to valid JS literal
+
+        :return: JS literal represented in a string
+        """
         if type(self.obj) in [str, unicode]:
             return repr(str(self.obj))
 
@@ -35,6 +65,10 @@ class JsArg(object):
 
 
 class JsFunction(object):
+    """
+    Callable object represents functions in window.WAPI
+    """
+
     def __init__(self, function_name, driver):
         self.driver = driver
         self.function_name = function_name
--- webwhatsapp.py
+++ webwhatsapp.py
@@ -1,4 +1,3 @@
-import os
 import time
 
 from selenium import webdriver
