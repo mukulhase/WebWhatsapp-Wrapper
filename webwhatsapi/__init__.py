9c970c49f8038a413b416a435b97066cc66e0d12
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2017-11-02 22:06:27 -0200

    Organize imports

--- webwhatsapi/__init__.py
+++ webwhatsapi/__init__.py
@@ -2,15 +2,17 @@
 WhatsAPI module
 """
 
-import time
-import os
-from selenium import webdriver
 import datetime
+import os
+import time
+
 from bs4 import BeautifulSoup
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class WhatsAPIDriver(object):
     _PROXY = None
