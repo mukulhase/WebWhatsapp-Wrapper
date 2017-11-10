e3283f8ee5ac02c815fc292aae90546d771d0af6
codemanat <4796142+codemanat@users.noreply.github.com>
2017-11-10 20:20:04 +0100

    Update webwhatsapp.py

--- webwhatsapp.py
+++ webwhatsapp.py
@@ -8,35 +8,31 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 
-
 from message import Message, MessageGroup
 from chat import Chat
 from wapi_js_wrapper import WapiJsWrapper
 from consts import Selectors, URL
 
 
-
-
 class WhatsAPIDriver(object):
     def __init__(self, browser='firefox', username="API"):
         self.username = username
-        if browser.lower() == 'firefox':
+        self.browser_type = browser
+        if self.browser_type.lower() == 'firefox':
             # from https://github.com/siddhant-varma/WhatsAPI/blob/master/webwhatsapi/__init__.py
-            self.path = os.path.dirname(sys.argv[0]) + 'firefox_cache' + '/' + self.username
+            self.path = os.path.join(os.path.dirname(sys.argv[0]), 'firefox_cache', self.username)
             if not os.path.exists(self.path):
                 os.makedirs(self.path)
-            self.profile = webdriver.FirefoxProfile(self.path)
-            #self._driver = webdriver.Firefox()
-            self._driver = webdriver.Firefox(self.profile)
-        if browser.lower() == 'chrome':
-            self.chrome_options = Options()
-            self.chrome_options.add_argument("user-data-dir=" + os.path.dirname(sys.argv[0]) + 'chrome_cache' + '/' +
-                                             self.username )
-            self._driver = webdriver.Chrome(chrome_options=self.chrome_options)
+            self._firefox_profile = webdriver.FirefoxProfile(self.path)
+            self._driver = webdriver.Firefox(self._firefox_profile)
 
+        if self.browser_type.lower() == 'chrome':
+            self._chrome_options = Options()
+            self._chrome_options.add_argument("user-data-dir=" + os.path.join(os.path.dirname(sys.argv[0]), 'chrome_cache',
+                                             self.username))
+            self._driver = webdriver.Chrome(chrome_options=self._chrome_options)
 
         self.wapi_functions = WapiJsWrapper(self._driver)
-
 
         # Open page
         self._driver.get(URL)
