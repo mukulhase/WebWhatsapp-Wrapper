d13949d015f0f7eb53b8c90627d168c4829a5b13
Meshulam Silk <moomoohk@ymail.com>
2017-11-11 19:05:08 +0200

    Documentation

--- objects/whatsapp_object.py
+++ objects/whatsapp_object.py
@@ -1,4 +1,12 @@
 def driver_needed(func):
+    """
+    Decorator for WhatsappObject methods that need to communicate with the browser
+
+    It ensures that the object receives a driver instance at construction
+
+    :param func: WhatsappObject method
+    :return: Wrapped method
+    """
     def wrapped(self, *args):
         if not self._driver:
             raise AttributeError("No driver passed to object")
@@ -9,7 +17,24 @@ def driver_needed(func):
 
 
 class WhatsappObject(object):
+    """
+    Base class for Whatsapp objects
+
+    Intended to wrap JS objects fetched from the browser
+
+    Can also be used as an interface to operations (such as sending messages to chats)
+    To enable this functionality the constructor must receive a WhatsAPIDriver instance
+    """
+
     def __init__(self, js_obj, driver=None):
+        """
+        Constructor
+
+        :param js_obj: Whatsapp JS object to wrap
+        :type js_obj: dict
+        :param driver: Optional driver instance
+        :type driver: WhatsAPIDriver
+        """
         self.id = js_obj["id"]
         self.name = js_obj["name"]
 
