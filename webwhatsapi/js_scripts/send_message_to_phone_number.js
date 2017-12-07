ca7553f219534eedf86b6ade4cee8ba41010bf0e
giovanny <giovanny@messoft.com>
2017-12-07 11:34:13 -0600

    Before search phone in Store.Contact.models check if that contact is a User, cause there are another different items in Store.Contact.models, and this can produce a undefined when you try e.__x_id.search(contact) != -1

--- webwhatsapi/js_scripts/send_message_to_phone_number.js
+++ webwhatsapi/js_scripts/send_message_to_phone_number.js
@@ -2,16 +2,21 @@ var Chats = Store.Chat.models;
 var contact = arguments[0];
 var message = arguments[1];
 
-var user = Store.Contact.models.find(function (e) { return e.__x_id.search(contact)!=-1 });
+var user = Store.Contact.models.find(function(e) {
+    if(e.isUser == true) {
+        return e.__x_id.search(contact) != -1
+    }
+});
 
 if(!user) return;
 
 Store.Chat.add({ id: user.__x_id, }, { merge: true, add: true, });
 
 for (chat in Chats) {
-    if (isNaN(chat)) {
+    if (isNaN(chat) || Chats[chat].__x_id.indexOf(contact) == -1) {
         continue;
     };
+
     var temp = {};
     temp.contact = Chats[chat].__x_formattedTitle;
     temp.id = Chats[chat].__x_id;
