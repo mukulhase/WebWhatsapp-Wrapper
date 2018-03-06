fb341a944cc708b9c4601aac4e34aec56cd169bc
alfred82santa <alfred.santacatalinagea@telefonica.com>
2018-03-06 16:38:42 +0100

    Upsss it is a message

--- webwhatsapi/__init__.py
+++ webwhatsapi/__init__.py
@@ -407,7 +407,7 @@ class WhatsAPIDriver(object):
         result = self.wapi_functions.sendMessage(chat_id, message)
 
         if result:
-            return factory_chat(result, self)
+            return factory_message(result, self)
         return False
 
     def send_message_to_id(self, recipient, message):
