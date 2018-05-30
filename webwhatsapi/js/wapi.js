e9c11a2af1b89b899440d3296d093d2ae08132ef
bobaoapae <33332428+bobaoapae@users.noreply.github.com>
2018-05-30 16:08:31 -0300

    Restore Store.Conn

--- webwhatsapi/js/wapi.js
+++ webwhatsapi/js/wapi.js
@@ -91,6 +91,8 @@ window.WAPI._serializeMessageObj = (obj) => {
 if (window.Store === undefined) {
     webpackJsonp([], {"bcihgfbdeb": (x, y, z) => window.Store = z('"bcihgfbdeb"')}, "bcihgfbdeb");
     webpackJsonp([], {"iaeeehaci": (x, y, z) => window.Store.Wap = z('"iaeeehaci"')}, "iaeeehaci");
+    webpackJsonp([], {"jfefjijii": (x, y, z) => window.Store.Conn = z('"jfefjijii"')}, "jfefjijii");
+    window.Store.Conn = window.Store.Conn.default;
 }
 
 
