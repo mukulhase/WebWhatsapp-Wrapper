1c9d0d127250bd8e6bc6c31126e053d9de508e90
Bruno Soares da Silva <brunosoares.inf@gmail.com>
2018-06-04 17:46:03 -0300

    - Fixing length method in python and array listener definition

--- webwhatsapi/wapi_js_wrapper.py
+++ webwhatsapi/wapi_js_wrapper.py
@@ -134,7 +134,7 @@ class NewMessagesObservable(Thread):
         while True:
             try:
                 new_js_messages = self.wapi_js_wrapper.getBufferedNewMessages()
-                if isinstance(new_js_messages, (collections.Sequence, np.ndarray)) and new_js_messages.length > 0:
+                if isinstance(new_js_messages, (collections.Sequence, np.ndarray)) and len(new_js_messages) > 0:
                     new_messages = []
                     for js_message in new_js_messages:
                         new_messages.append(factory_message(js_message, self.wapi_driver))
