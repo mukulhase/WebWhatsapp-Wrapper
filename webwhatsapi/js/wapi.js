15743135c389f5f863bc2829ee53094c8d596eaf
João Vitor Borges <33332428+bobaoapae@users.noreply.github.com>
2018-06-28 13:32:36 -0300

    Update sendMessageWithThumb

--- webwhatsapi/js/wapi.js
+++ webwhatsapi/js/wapi.js
@@ -332,26 +332,17 @@ window.WAPI.sendImageFromDatabasePicBot = function (picId, chatId, caption) {
     return true;
 };
 
-window.WAPI.sendMessageWithThumb = function (thumb, url, title, description, chatId, done) {
+window.WAPI.sendMessageWithThumb = function (thumb, url, title, description, chatId) {
     var chatSend = WAPI.getChat(chatId);
     if (chatSend === undefined) {
         return false;
     }
-    var msgWithImg = chatSend.createMessageFromText(".");
-    msgWithImg.__x_hasLink = title;
-    msgWithImg.__x_body = description + '\n ' + url;
-    msgWithImg.__x_isLink = title;
-    msgWithImg.__x_description = description;
-    msgWithImg.__x_subtype = 'url';
-    msgWithImg.__x_title = title;
-    msgWithImg.__x_thumbnail = thumb;
-
-    chatSend.addAndSendMsg(msgWithImg);
-
-    if(done!==undefined){
-        done(true);
-    }
-
+    var linkPreview = {canonicalUrl: url,
+        description: description,
+        matchedText: url,
+        title: title,
+        thumbnail: thumb};
+    chatSend.sendMessage(url, {linkPreview: linkPreview, mentionedJidList: [], quotedMsg: null, quotedMsgAdminGroupJid: null});
     return true;
 };
 
