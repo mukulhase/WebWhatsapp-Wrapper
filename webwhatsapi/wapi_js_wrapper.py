b4898c8916da43d5bbaeeeef84f73d72fd26c806
Marcelo Maia <mmaia.cc@gmail.com>
2019-11-27 22:58:51 -0300

    flake8 applied

--- webwhatsapi/wapi_js_wrapper.py
+++ webwhatsapi/wapi_js_wrapper.py
@@ -64,7 +64,8 @@ class WapiJsWrapper(object):
         except NameError:
             script_path = os.getcwd()
 
-        result = self.driver.execute_script("if (document.querySelector('*[data-icon=chat]') !== null) { return true } else { return false }")
+        result = self.driver.execute_script(
+            "if (document.querySelector('*[data-icon=chat]') !== null) { return true } else { return false }")  # noqa E501
         if result:
             with open(os.path.join(script_path, "js", "wapi.js"), "r") as script:
                 self.driver.execute_script(script.read())
@@ -137,7 +138,8 @@ class JsFunction(object):
                 retry_command.is_a_retry = True
                 retry_command(*args, **kwargs)
             else:
-                raise JsException("Error in function {0} ({1}). Command: {2}".format(self.function_name, e.msg, command))
+                raise JsException(
+                    "Error in function {0} ({1}). Command: {2}".format(self.function_name, e.msg, command))
         except WebDriverException as e:
             if e.msg == 'Timed out':
                 raise WapiPhoneNotConnectedException("Phone not connected to Internet")
@@ -165,7 +167,7 @@ class NewMessagesObservable(Thread):
                         new_messages.append(factory_message(js_message, self.wapi_driver))
 
                     self._inform_all(new_messages)
-            except Exception as e:
+            except Exception as e:  # noqa F841
                 pass
 
             time.sleep(2)
